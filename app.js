2fa052c1fd89c4cc37b8844aa029e482755e30c9
colombianet <oscar_p1986@hotmail.com>
2020-05-05 18:52:41 -0500

    Agregamos configuración del puerto para que funcione en local y en produccion

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ app.use(express.static(__dirname + '/public'));
 app.set('view engine', 'hbs');
 const hbs = require('hbs');
 
+const puerto = process.env.PORT || 3000;
+
 hbs.registerHelper('getAnio', () => new Date().getFullYear());
 hbs.registerPartials(__dirname + '/views/parciales');
 
@@ -19,6 +21,6 @@ app.get('/about', function(req, res) {
     res.render('about')
 });
 
-app.listen(3000, () => {
-    console.log('Escuchando el puerto 3000');
+app.listen(puerto, () => {
+    console.log(`Escuchando el puerto ${puerto}`);
 });
